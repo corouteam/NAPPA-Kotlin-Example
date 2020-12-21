e82c68cb9c35e232ce29b54fb89fad818bc80189
Paolo Rotolo <paolo@rotolo.dev>
2020-12-21 19:47:27 +0100

    Add packages

--- app/src/main/java/com/github/corouteam/nappa_kotlin_test/activity/MainActivity.kt
+++ app/src/main/java/com/github/corouteam/nappa_kotlin_test/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.corouteam.nappa_kotlin_test
+package com.github.corouteam.nappa_kotlin_test.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.corouteam.nappa_kotlin_test.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
