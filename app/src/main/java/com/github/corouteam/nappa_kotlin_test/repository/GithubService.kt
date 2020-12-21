3c491f646e7763d1e65aa570ecd189c75e9b3bfa
Paolo Rotolo <paolo@rotolo.dev>
2020-12-21 19:44:25 +0100

    Initial commit

---
+++ app/src/main/java/com/github/corouteam/nappa_kotlin_test/repository/GithubService.kt
@@ -0,0 +1,12 @@
+package com.wideverse.nappa_kotlin_test
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface GithubService {
+    @GET("users/{user}/repos")
+    fun listRepos(@Path("user") user: String?): Call<ResponseBody>
+}
---
+++ app/src/main/java/com/github/corouteam/nappa_kotlin_test/repository/RetrofitBuilder.kt
@@ -0,0 +1,27 @@
+package com.wideverse.nappa_kotlin_test
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import nl.vu.cs.s2group.nappa.*
+import okhttp3.OkHttpClient
+
+class RetrofitBuilder {
+    companion object {
+        private var retrofitInstance: Retrofit? = null
+        private var service: GithubService? = null
+    }
+
+    fun getGithubService(): GithubService {
+        if (retrofitInstance == null) {
+            retrofitInstance = Retrofit.Builder()
+            .client(Nappa.getOkHttp(OkHttpClient()))
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            service = retrofitInstance!!.create(GithubService::class.java)
+        }
+
+        return service!!
+    }
+}
