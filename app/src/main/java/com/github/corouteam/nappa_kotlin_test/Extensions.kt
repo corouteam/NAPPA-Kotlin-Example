33b5bb9c0966658bc2fc4e0ec9cfc3a7104a141f
Andrea Lops <alops@host-87-3-93-216.retail.telecomitalia.it>
2020-12-21 22:45:35 +0100

    Add model

--- app/src/main/java/com/github/corouteam/nappa_kotlin_test/Extensions.kt
+++ app/src/main/java/com/github/corouteam/nappa_kotlin_test/Extensions.kt
@@ -1,6 +1,11 @@
 package com.github.corouteam.nappa_kotlin_test
 
+import com.github.corouteam.nappa_kotlin_test.model.Commit
 import com.github.corouteam.nappa_kotlin_test.model.ListItem
 import com.github.corouteam.nappa_kotlin_test.model.Organization
 
-fun List<Organization>.asListItem() = map { ListItem(it.name, it.description, it.public_repos) }
+fun Organization.asListItem() = ListItem(name, description, public_repos)
+
+fun Commit.asListItem() = ListItem(author, message, )
+
+
